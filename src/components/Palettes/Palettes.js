6277039e1b1c584b0db91101d4933bfc9ea327ac
Lauren Boyer <lboyer4@outlook.com>
2019-07-02 18:56:44 -0600

    added a function that randomly generates color codes

---
+++ src/components/Palettes/Palettes.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './_Palettes.scss';
+
+class Palettes extends Component {
+	constructor() {
+		super();
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Palettes</h1>
+			</div>)
+	}
+}
+
+export default Palettes;
