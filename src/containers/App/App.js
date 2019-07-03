48d42a555af56af19acb5339b6286641e926aa7f
Lauren Boyer <lboyer4@outlook.com>
2019-07-02 19:37:51 -0600

    changed file structure to make use of containers directory

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './_App.scss';
-import { Header } from '../Header/Header.js';
+import { Header } from '../../components/Header/Header.js';
 import Swatch from '../Swatch/Swatch.js';
-import Projects from '../Projects/Projects.js';
-import Palettes from '../Palettes/Palettes.js';
+import Projects from '../../components/Projects/Projects.js';
+import Palettes from '../../components/Palettes/Palettes.js';
 
 class App extends Component {
 	constructor() {
