03a53f14488985fb22fae52ade8d6d971f6f92df
Sally Haefling <sallyhaefling@gmail.com>
2019-07-08 17:00:02 -0600

    Add deleteProject and addProject and setProjects tests for project reducer

--- src/reducers/project-reducer.test.js
+++ src/reducers/project-reducer.test.js
@@ -8,5 +8,56 @@ describe('project', () => {
     const result = project(undefined, [])
 
     expect(result).toEqual(expected)
+  });
+
+  it('should return the state with all projects', () => {
+    const projects = [
+      {
+      name: "Project 1"
+    },
+    {
+      name: "Project 2"
+    }
+  ]
+    const expected = projects
+
+
+  const result = project(undefined, actions.setProjects(projects))
+
+  expect(result).toEqual(expected)
+  });
+
+  it('should return state with new project', () => {
+    const mockProject = [{ name: "Project 3" }]
+    
+    const expected = mockProject
+
+    const result = project(undefined, actions.addProject(mockProject))
+
+    expect(result[0]).toEqual(expected)
+  });
+
+  it('should delete a project', () => {
+    const mockProject = [
+      {
+      name: "Project 12" ,
+      id: 10 
+    },
+    {
+      name: "Project 17" ,
+      id: 13 
+    }
+  ]
+
+  const expected = [
+    {
+      name: "Project 12",
+      id: 10
+    }
+  ]
+
+    const result = project(mockProject, actions.deleteProject(13))
+
+    expect(result).toEqual(expected)
   })
 })
