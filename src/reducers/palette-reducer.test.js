650244f38172fe87a4e2264a7c73b8ac8869fbe3
Sally Haefling <sallyhaefling@gmail.com>
2019-07-08 16:03:42 -0600

    Add makePalette test for palette reducer

--- src/reducers/palette-reducer.test.js
+++ src/reducers/palette-reducer.test.js
@@ -1,12 +1,22 @@
-import makePalette from './palette-reducer';
+import palette from './palette-reducer';
 import * as actions from '../actions';
 
 describe('palette', () => {
   it('should return initial state', () => {
     const expected = [];
 
-    const result = makePalette(undefined, [])
+    const result = palette(undefined, [])
 
     expect(result).toEqual(expected)
-  })
+  });
+
+  it('should return the state with a new palette',  () => {
+    const expected = {
+      color: "00FF00"
+    }
+
+    const result = palette(undefined, actions.makePalette(expected))
+
+    expect(result).toEqual(expected)
+  });
 })
