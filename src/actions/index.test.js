e25bdc907a00644320d217aa0659e04be2df8302
Sally Haefling <sallyhaefling@gmail.com>
2019-07-08 15:15:46 -0600

    Add packages for enzyme and jest

---
+++ src/actions/index.test.js
@@ -0,0 +1,9 @@
+import * as actions from './index';
+
+describe('actions', () => {
+  it('should return palette with a type of MAKE_PALETTE', () => {
+    const palette = [{
+      
+    }]
+  })
+})
