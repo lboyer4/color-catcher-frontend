a511bfde857579c72b5aa5eae0eaf186cf436e5f
Lauren Boyer <lboyer4@outlook.com>
2019-07-09 21:27:05 -0600

    Removed console logs and added some semicolons and removed the 0 that was showing up

--- src/components/Palettes/Palettes.js
+++ src/components/Palettes/Palettes.js
@@ -7,69 +7,65 @@ export class Palettes extends Component {
 	constructor() {
 		super();
 		this.state = {
-			name: '',
-			colors: []
+			name: ''
 		}
 	}
 
 	handleChange = (e) => {
-		this.setState({name: e.target.value})
-	}
+		this.setState({name: e.target.value});
+	};
 
 	addPalette = () => {
 		const colors = this.props.palette.reduce((acc, colorObject, index) => {
-
 			acc[`color_${index+1}`] = colorObject.color.slice(1)
-			return acc
-		}, {})
+				return acc
+		}, {});
 		
 		const newPalette = {...colors, name: this.state.name, project_id: this.props.project.id}
-		this.props.addPalette(newPalette)
-		this.postPalette(newPalette)
-		}
+		this.props.addPalette(newPalette);
+		this.postPalette(newPalette);
+	};
 
-		postPalette = (palette) => {
-			let options = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(palette)
-    };
-			fetch('http://localhost:3001/api/v1/palettes', options)
-			.then(response => {
+	postPalette = (palette) => {
+		let options = {
+	    method: "POST",
+	    headers: { "Content-Type": "application/json" },
+	    body: JSON.stringify(palette)
+    }
+		fetch('http://localhost:3001/api/v1/palettes', options)
+		.then(response => {
 			if(!response.ok) {
 				throw Error('Error posting palette')
 			} else {
 				return response
 			}
 		})
-		}
+	};
 
 	render() {
-		const matchingPalettes = this.props.palettes.length && this.props.palettes.filter(palette => {
+		const matchingPalettes = this.props.palettes && this.props.palettes.filter(palette => {
 			return palette.project_id === this.props.project.id
-		})
+		});
 
 		const displayPalettes = matchingPalettes && matchingPalettes.map(palette => {
-				// let displayColors = ['#' + palette.color_1, '#' + palette.color_2, '#' + palette.color_3, '#' + palette.color_4,'#' +  palette.color_5]
-
-			return (
-				<div className='palette-container' key={palette.id}>
-					<h2>{palette.name}</h2>
-					<div className='color-holder'>
-					<div className='picked-color' style={{backgroundColor: `#${palette.color_1}`}}>
-					</div>
-					<div className='picked-color' style={{backgroundColor: `#${palette.color_2}`}}>
-					</div>
-					<div className='picked-color' style={{backgroundColor: `#${palette.color_3}`}}>
-					</div>
-					<div className='picked-color' style={{backgroundColor: `#${palette.color_4}`}}>
-					</div>
-					<div className='picked-color' style={{backgroundColor: `#${palette.color_5}`}}>
-					</div>
-					</div>
-			</div>
-			)
-		})
+				return (
+					<div className='palette-container' key={palette.id}>
+						<h2>{palette.name}</h2>
+						<div className='color-holder'>
+						<div className='picked-color' style={{backgroundColor: `#${palette.color_1}`}}>
+						</div>
+						<div className='picked-color' style={{backgroundColor: `#${palette.color_2}`}}>
+						</div>
+						<div className='picked-color' style={{backgroundColor: `#${palette.color_3}`}}>
+						</div>
+						<div className='picked-color' style={{backgroundColor: `#${palette.color_4}`}}>
+						</div>
+						<div className='picked-color' style={{backgroundColor: `#${palette.color_5}`}}>
+						</div>
+						</div>
+				</div>
+				);
+			});
 
 		return (
 			<div className='palette-holder'>
@@ -77,9 +73,10 @@ export class Palettes extends Component {
 				<input placeholder='Name your palette!' type='text' onChange={this.handleChange} value={this.state.name} />
 				<button onClick={this.addPalette}>Add Palette </button>
 				{displayPalettes}
-			</div>)
-	}
-}
+			</div>
+		);
+	};
+};
 
 export const mapStateToProps = (state) => ({
 	project: state.project,
